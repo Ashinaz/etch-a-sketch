053c47a14dae21bfc17fa148d3d1d979b261a3b3
ash <230867782+Ashinaz@users.noreply.github.com>
2026-09-20 00:42:12 +0800

    trigger events and functions when window loads
    call function gridX w/ grid argument to trigger a default grid of 64x64
    attach event listeners to container to setup for drawing on the canvas

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+window.addEventListener("load", function () {
+  gridX(grid)
+  container.addEventListener("mousedown", startDrawing)
+  container.addEventListener("mousemove", currentlyDrawing)
+  container.addEventListener("mouseup", stopDrawing)
+})
+
 const container = document.querySelector(".container")
 const canvas = document.querySelector("#canvas")
 const context = canvas.getContext("2d")
 canvas.height = container.offsetHeight
 canvas.width = container.offsetWidth
+let grid = 64
 
 function gridX(num) { 
   for (let i = 0; i < num; i++) {
