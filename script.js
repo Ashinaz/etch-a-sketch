8a971655e405dcf2ecd2a8a2e09b580dddfee4ac
ash <230867782+Ashinaz@users.noreply.github.com>
2026-09-19 22:08:08 +0800

    create first axis of grid

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const canvas = document.querySelector("#canvas")
 const context = canvas.getContext("2d")
 canvas.height = container.offsetHeight
 canvas.width = container.offsetWidth
+
+function gridX(num) { 
+  for (let i = 0; i < num; i++) {
+    let divGrid = document.createElement("div")
+    divGrid.classList.toggle("gridX")
+    gridY(num, divGrid)
+    container.appendChild(divGrid)
+  }
+}
