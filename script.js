76405a61862150c76d0f12cd08a17137614ba21d
ash <230867782+Ashinaz@users.noreply.github.com>
2026-09-19 22:05:23 +0800

    get container div and canvas
    get div container and canvas and its context
    also sets the height and width of the canvas the same as the container

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container")
+const canvas = document.querySelector("#canvas")
+const context = canvas.getContext("2d")
+canvas.height = container.offsetHeight
+canvas.width = container.offsetWidth
